3c13534ffa773280478aff2689fca23a648dd9fb
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-07-30 15:39:05 +0000

    chore: standardize DevOps config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "refactor", "chore", "test", "ci", "build"]],
+    "header-max-length": [2, "always", 72],
+    "subject-full-stop": [2, "never", "."],
+  },
 };
